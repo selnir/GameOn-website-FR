db7cb3db6a92058ad584dd807eaa18eaaad2bf8d
nelumbo <remi.chambiet@laposte.net>
2022-01-03 14:24:09 +0100

    debut de l'implementation du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,5 +19,36 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close the modal form
+function closemodal()
+{
+  modalbg.style.display = "none";
+}
 
+// validity check of the form on submit
+function validate(){
+  var firstname=document.forms["reserve"]["first"];
+  var familyname=document.forms["reserve"]["last"];
+  var e_mail=document.forms["reserve"]["email"];
+  var birth_date=document.forms["reserve"]["birthdate"];
+  var quantity=document.forms["reserve"]["quantity"];
+  var checkemail=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
 
+  if (firstname.value.lenght==""||firstname.value.lenght <= 2 )
+  {
+    alert("Mettez votre prenom.");
+    firstname.focus();
+    return false;
+  }
+  if (familyname.value.lenght==""||familyname.value.lenght <= 2)
+  {
+    alert("Mettez votre prenom.");
+    familyname.focus();
+    return false;
+  }
+  if (checkemail.test(e_mail.value))
+  {
+
+  }
+}
